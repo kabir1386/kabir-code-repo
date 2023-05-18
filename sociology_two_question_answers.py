de16edd8eabf5f5ff1f63e145738d31f78a13989
kabir-janta-ka-aaina-pc <kabir1386@gmail.com>
2023-05-18 17:22:01 +0530

    code modified

--- sociology_two_question_answers.py
+++ sociology_two_question_answers.py
@@ -4,7 +4,7 @@ import sys
 
 #I KNOW THAT IT WAS NOT NECESSARY TO ADD ALL THE CODE IN ONE FUNCTION, BUT I FOUND THIS METHOD EASY THAT IS WHY I DID IT. YOU CAN SUGGEST ME SOME MORE WAYS TO MAKE THE CODE MORE SIMPLER! 
 
-start = input("do you want to play a game of answering some simple questions of Sociology? (make sure you type the spelling correctly when answering) \n\nif yes, then type 'yes':")
+start = input("do you want to play a simple game of answering some general knowledge questions? (make sure you type the spelling correctly when answering) \n\nif yes, then type 'yes':")
 if start != "yes":
     sys.exit()
 
@@ -12,17 +12,17 @@ def questions():
     print()
     print("Answer the following questions in one or two word - ")
     print()
-    a = input("1. state the two types of laws in sociology in context with norms.\nanswer - ")
+    a = input("1. What is the capital of France?.\nanswer - ")
     print()
-    b = input("2. villages are a part of _______ community. \nanswer - ")
+    b = input("2. What is the capital of Nepal?. \nanswer - ")
     print()
     c = input("3. who was the father of communism?. \nanswer - ")
     print()
-    if a == "customary and enacted law":
+    if a == "paris":
         print("1. correct!")
     else:
         print("1. wrong, if you think its correct, please check the spelling and sentence structure!")
-    if b == "rural":
+    if b == "kathmandu":
         print("2. correct!")
     else:
         print("2. wrong, if you think its correct, please check the spelling and sentence structure!")
@@ -30,7 +30,7 @@ def questions():
         print("3. correct!")
     else:
         print("3. wrong, if you think its correct, please check the spelling and sentence structure!")
-    if a != "customary and enacted law" and b != "rural":
+    if a != "paris" and b != "kathmandu":
         print("sorry, all are wrong!")
 
 
