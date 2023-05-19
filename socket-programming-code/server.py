5b2213fa4f04ff2eae08494a0fe813eec015d1d5
kabir1386 <70250507+kabir1386@users.noreply.github.com>
2023-05-19 19:04:15 +0530

    Add files via upload

---
+++ socket-programming-code/server.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost',9999))
+
+s.listen(3)
+print('waiting for connections')
+
+while True:
+    c, addr = s.accept()
+    name = c.recv(1024).decode()
+    print("Connected with ", addr, name)
+
+    c.send(bytes("Welcome to the server created by Kabir",'utf-8'))
+
+    c.close()
